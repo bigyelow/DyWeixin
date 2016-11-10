75671e6944c459991e880aa2f711a4c1525b1eee
huangduyu <huangduyu@douban.com>
2016-11-10 14:54:08 +0800

    config header file import

--- DyWeixin/DyWeixin.h
+++ DyWeixin/DyWeixin.h
@@ -15,5 +15,7 @@ FOUNDATION_EXPORT double DyWeixinVersionNumber;
 FOUNDATION_EXPORT const unsigned char DyWeixinVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DyWeixin/PublicHeader.h>
-
+#import <DyWeixin/WXApi.h>
+#import <DyWeixin/WXApiObject.h>
+#import <DyWeixin/WechatAuthSDK.h>
 
